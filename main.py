ff5eb1dcc4e92951d1097e40a7229fe3d1f574d0
flsteele <136656952+flsteele@users.noreply.github.com>
2023-07-05 23:56:41 +0000

    added code

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-#your code here!
+miles_driven = float(input('Enter Miles driven: '))
+gallons_used = float(input('Enter gallons Used: '))
+
+mpg = miles_driven/gallons_used
+
+print('Miles per gallon = ', mpg)#your code here!
 
 #read in the miles and read in the gallons used.
 
